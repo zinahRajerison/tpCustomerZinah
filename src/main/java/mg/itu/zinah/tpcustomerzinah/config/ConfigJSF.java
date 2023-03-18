2507e5abc006bc659dc3dbf1e09de786b66c0d15
zinahRajerison <zinahrajerison2000@gmail.com>
2023-03-18 18:59:57 +0100

    Ajout page customerList.xhtml +rectification des imports en jakarta

---
+++ src/main/java/mg/itu/zinah/tpcustomerzinah/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.zinah.tpcustomerzinah.config;
+
+/**
+ *
+ * @author Lenovo
+ */
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
---
+++ src/main/java/mg/itu/zinah/tpcustomerzinah/jsf/CustomerBean.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.zinah.tpcustomerzinah.jsf;
+
+import jakarta.ejb.EJB;  
+import jakarta.inject.Named;  
+import jakarta.faces.view.ViewScoped;  
+import java.io.Serializable;  
+import java.util.List;  
+import mg.itu.zinah.tpcustomerzinah.ejb.CustomerManager;
+import mg.itu.zinah.tpcustomerzinah.entities.Customer;
+
+/**
+ * Backing bean de la page customerList.xhtml.
+ */
+@Named(value = "customerBean")  
+@ViewScoped  
+public class CustomerBean implements Serializable {  
+  private List<Customer> customerList;  
+
+  @EJB
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable.
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
